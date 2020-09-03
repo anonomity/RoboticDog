78c3a6c43d1ba74141a5e6b9d1a2b4e59875c867
Jackie <jlauravelez@gmail.com>
2020-09-03 14:36:25 +0200

    roboticdog content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-
+import Image from "../components/image"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Woof!</h1>
+    <p>Welcome to RoboticDog</p>
+    <p>Are you ready to explore the world of programming?</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
