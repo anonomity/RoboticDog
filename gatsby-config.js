0496021bddcf06e907a8f7a44ba91fe159dd81ed
Jackie <jlauravelez@gmail.com>
2020-09-03 14:35:11 +0200

    better SEO results

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `RoboticDog`,
+    description: `Dedicated to teaching you programming`,
+    author: `Jackelyn Velez and Staszek Stępień`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
