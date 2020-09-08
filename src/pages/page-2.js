ab627799191b9524324fea00a0a020d267182fc2
Jackie <jlauravelez@gmail.com>
2020-09-08 19:34:29 +0200

    list of classes

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,20 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Lessons</h1>
+    <p>Here are a list of our lessons</p>
+    <ol>
+      <h3>September</h3>
+      <li>Lesson 1</li>
+      <li>Lesson 2</li>
+      <li>Lesson 3</li>
+      <h3>October</h3>
+      <li>Lesson 4</li>
+      <li>Lesson 5</li>
+      <li>Lesson 6</li>
+      <li>Lesson 7</li>
+  
+    </ol>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
