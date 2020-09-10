9a1aa2b04029a37a8dae3888991b1eece9588989
anonomity <jlauravelez@gmail.com>
2020-09-10 10:37:42 +0200

    converting from MD to MDX

---
+++ src/BlogUI/CodeJs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Code(){
+    return(
+        <div style={{backgroundColor: 'red', border: '3px solid black'}}>
+            <p>const name= 'Jackie'</p>
+        </div>
+    )
+}
