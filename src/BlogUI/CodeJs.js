81f3fa40faf658469e9545ab567cd597d5c51a6e
Jackelyn Velez <jlauravelez@gmail.com>
2020-09-10 12:41:53 +0200

    settle warnings

--- src/BlogUI/CodeJs.js
+++ src/BlogUI/CodeJs.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default function Code(){
-    return(
-        <div style={{backgroundColor: 'red', border: '3px solid black'}}>
-            <p>const name= 'Jackie'</p>
-        </div>
-    )
+class CodeJs extends Component{
+    render(){
+        return(
+            <div style={{backgroundColor: 'red', border: '3px solid black'}}>
+                <p>const name= 'Jackie'</p>
+            </div>
+        )
+    }
 }
+export default CodeJs
