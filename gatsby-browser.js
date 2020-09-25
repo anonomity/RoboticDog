2d65e290a1fdfcf54a3798f770062a2eca71577c
Jackelyn Velez <jlauravelez@gmail.com>
2020-09-25 11:28:42 +0200

    post: Create lesson 2 modify lesson 1

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,13 +7,15 @@ import Activity from "./src/components/ui/activity";
 const MyH2 = (props) => <Activity {...props}/>
 const MyH3 = (props) => <h3 style={{textAlign: 'center'}} {...props}/>
 const P = (props) => <p style={{textAlign: 'center'}} {...props}/>
+const OL = (props) => <ol style={{textAlign: 'center', listStylePosition: 'inside'}} {...props}/>
 
 // #TODO: Make Rules / Explaining Activity Component
 
 const components = {
     h2: MyH2,
     h3: MyH3,
-    p: P
+    p: P,
+    ol: OL
   }
 
 export const wrapRootElement =({ element }) => {
